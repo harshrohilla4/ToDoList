25fd09f49ccf57b716b5c64280e94e63fa6b4ca1
harshvardhan rohilla <harsh.rohilla4@gmail.com>
2022-01-09 21:57:37 +0530

    Storing on server + delete feature

--- date.js
+++ date.js
@@ -1,21 +1,27 @@
-//jshint version:6
-module.exports.getDate = function () {
+//jshint esversion:6
+
+exports.getDate = function() {
+
   const today = new Date();
+
   const options = {
     weekday: "long",
     day: "numeric",
-    month: "long",
+    month: "long"
   };
 
   return today.toLocaleDateString("en-US", options);
+
 };
 
-module.exports.getDay= function(){
-    const today = new Date();
-    const options = {
-      weekday: "long",
-      
-    };
-  
-    return today.toLocaleDateString("en-US", options);
-}
+exports.getDay = function () {
+
+  const today = new Date();
+
+  const options = {
+    weekday: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+};
